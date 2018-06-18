496260af5976c8efb89b47ba3c418bd99ae697e8
shubhamsapra97 <shubham.sapra97@gmail.com>
2018-06-19 00:03:48 +0530

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -12,16 +12,6 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({description:'water bill' , note:'sfsd' , amount:1500 , createdAt:200}));
-store.dispatch(addExpense({description:'gas bill' , note:'sfsd' , amount:100 , createdAt:800}));
-store.dispatch(addExpense({description:'rent' , note:'sfsd' , amount:2000 , createdAt:500}));
-
-store.dispatch(setTextFilter());
-
-const visibleExpenses = getVisibleExpenses(store.getState().expenses , store.getState().filters);
-
-console.log(visibleExpenses);
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,51 @@
 const path = require('path');
-console.log(__dirname);
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname,'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = (env) =>{
+
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname,'public'),
+            filename: 'bundle.js'
         },
-        {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname,'public'),
-        historyApiFallback: true
-    }
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            },
+            {
+                test: /\.s?css$/,
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMaps: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMaps: true
+                            }
+                        }
+                    ]
+                })
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname,'public'),
+            historyApiFallback: true
+        }
+    };
+
 };
